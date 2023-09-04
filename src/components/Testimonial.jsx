72320a5a308c27fae0510ac60eb7b949d1a18a93
raihanromzi <raihan.rakhman16@gmail.com>
2023-09-05 05:19:31 +0700

    hide next and previous button carousel

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,8 +1,8 @@
-'use client'
+'use client';
 
-import Slider from "react-slick";
-import testimonial from "@/data/testimonial";
-import Image from "next/image";
+import Slider from 'react-slick';
+import testimonial from '@/data/testimonial';
+import Image from 'next/image';
 
 const Testimonial = () => {
   var settings = {
@@ -12,6 +12,8 @@ const Testimonial = () => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    nextArrow: <br />,
+    prevArrow: <br />,
     responsive: [
       {
         breakpoint: 992,
@@ -40,12 +42,12 @@ const Testimonial = () => {
     <>
       <Slider {...settings}>
         {testimonial.slice(0, 4).map((item) => (
-          <div className="item" key={item.id}>
-            <div className="rating-home-block">
-              <div className="top-header d-flex flex-row-reverse">
-                <img src="images/icon/icon_112.svg" alt="" width={30} />
+          <div className='item' key={item.id}>
+            <div className='rating-home-block'>
+              <div className='top-header d-flex flex-row-reverse'>
+                <img src='images/icon/icon_112.svg' alt='' width={30} />
               </div>
-              <p className="tx-dark">{item.text}</p>
+              <p className='tx-dark'>{item.text}</p>
               {/* <div>
                   <ul className="style-none d-flex rating pt-15">
                     {Array.from({ length: item.rating }).map((_, index) => (
@@ -55,17 +57,17 @@ const Testimonial = () => {
                     ))}
                   </ul>
                 </div> */}
-              <div className="d-flex align-items-center">
-              <Image
+              <div className='d-flex align-items-center'>
+                <Image
                   width={40}
                   height={40}
-                  alt="testimonial avatar"
+                  alt='testimonial avatar'
                   src={item.image}
-                  className="rounded-circle"
+                  className='rounded-circle'
                 />
-                <div className="cost fw-500 tx-dark fs-10 p-2">
-                  Peserta {" "} {item.author},{" "}
-                  <span className="opacity-50 fw-normal fs-15">{item.colleague}</span>
+                <div className='cost fw-500 tx-dark fs-10 p-2'>
+                  Peserta {' '} {item.author},{' '}
+                  <span className='opacity-50 fw-normal fs-15'>{item.colleague}</span>
                 </div>
               </div>
             </div>
